db62893862c4d5430778e50cfd6224ee6319d4bd
powershellcrack <richard.tracy@hotmail.com>
2024-04-16 18:10:37 -0400

    updates to tests

--- Tests/ChangePrimaryUserAutomation.tests.ps1
+++ Tests/ChangePrimaryUserAutomation.tests.ps1
@@ -1,45 +1,133 @@
-$DeviceUpdateList = @(
-    'DTOLAB-WKHV001',
-    'DTOLAB-WKHV002'
-)
+#import device
+[string]$ResourcePath = ($PWD.ProviderPath, $PSScriptRoot)[[bool]$PSScriptRoot]
+$devicList = Import-Csv "$ResourcePath\tests\devicelist.sample.csv"
+#$devicList = Import-Csv "$ResourcePath\devicelist.csv"
 
-#user id
-$AssignedUser = "tracyr@dtolab.ltd" 
+#Import App Secret
+$app = Import-Clixml "$ResourcePath\tests\dtolab_intuneapp_secret.xml"
 
-# Set to $True to update the profile, $False to just see what would happen
-$DoProfileUpdate = $False
-#MAIN
+<#
+#set the admin check in regex
+^ = starts with
+adm- = the prefix for admin accounts
+#>
+$admincheck = '^adm-'
+
+<# TESTS AGAINST REGEX
+'adm-frankjones@dtolab.ltd' -match $admincheck
+'adam.tool@dtolab.ltd' -match $admincheck
+'admintools@dtolab.ltd' -match $admincheck
+#>
+
+#Set to true to reassign non-admin users. 
+#$Otherwise assigned users in list are ignored unless there is an admin account assigne to device
+$AssignNonAdminUsers = $false
+
+#IMPORT MODULES
 #==============================================
 
 Install-Module Az.Accounts
 Install-Module Microsoft.Graph.Authentication
 Install-Module Microsoft.Graph.Applications
-Install-Module IDMCmdlets -MinimumVersion 1.0.2.2 -Force
+Install-Module IDMCmdlets
 
-#import app secret
-$app = Import-Clixml .\dtolab_intuneapp_secret.xml
-$AppSecret = ConvertFrom-SecureString -SecureString $app.AppSecret -AsPlainText
-
+#MAIN
+#==============================================
+#set the graph endpoint
 $Global:GraphEndpoint = 'https://graph.microsoft.com'
-$Global:AuthToken = Get-IDMGraphAppAuthToken @app
-Connect-IDMGraphApp -AppAuthToken $Global:AuthToken
 
-#Within Autopilot:
-$AppId = '009bbee5-3ff5-4fe1-a2bd-2e0cec916eac'
-#STEP 1 - create random passphase (256 AES). Save the output as a variable (copy/paste)
+#connect to the graph using app
+
+<#
+#NOTE: The following code is used to generate the AES key and encrypted app secret
+STEP 1 - Create graph app with Intune permissions as hashtable (for splatting)
+$app = New-IDMGraphApp -appNamePrefix "IntuneDeviceManagerApp" -AsHashTable
+#save appdetails for later use (STORE SECURELY)
+$app | Export-Clixml .\intuneapp_secret.xml
+
+#STEP 2 - create random passphase (256 AES). Save the output as a variable (copy/paste)
 #NOTE: this key is unique; the same key must be used to decrypt
 $AESKey = New-Object Byte[] 32
 [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($AESKey)
 Write-host ('$AESKey = @(' + ($AESKey -join ",").ToString() + ')')
 
-$AESKey = @(28,89,174,82,179,115,230,166,2,32,44,75,32,210,236,121,46,61,151,176,111,48,49,108,251,205,33,18,11,178,25,121)
-#STEP 2 - Encrypt password with AES key. Save the output as a variable (copy/paste)
-$AppSecret = ConvertTo-SecureString -String $AppSecret -AsPlainText -Force | ConvertFrom-SecureString -Key $AESKey
-Write-host ('$ADEncryptedPassword = "' + $AppSecret + '"')
+#STEP 3 - Encrypt password with AES key. Save the output as a variable (copy/paste)
+$AppSecret = ConvertTo-SecureString -String $app.AppSecret -AsPlainText -Force | ConvertFrom-SecureString -Key $AESKey
+Write-host ('$AppSecretHashed = "' + $AppSecret + '"')
+#>
 
-#STEP 3 - Store as useable credentials; converts encrypted key into secure key for use (used in the script)
-$SecurePass = $AppSecret  | ConvertTo-SecureString -Key $AESKey
-$credential = New-Object System.Management.Automation.PsCredential($AppId, $SecurePass)
+$Global:GraphEndpoint = 'https://graph.microsoft.com'
+#From clixml input
+#$Global:AuthToken = Get-IDMGraphAppAuthToken @app
 
-#STEP 4 - Test password output (clear text) from creds
-$credential.GetNetworkCredential().password
+$AESKey = '<generated-aes-key (step 1)>'
+
+$TenantId = '<your-tenant-id>'
+$AppId = '<your-app-id>'
+$AppSecretHashed = '<encrypted-app-secret>'
+$Global:AuthToken = Get-IDMGraphAppAuthToken -AppId $AppId -AppSecret $AppSecretHashed -TenantId $TenantId -AESKey $AESKey -CloudEnvironment Public
+Connect-IDMGraphApp -AppAuthToken $Global:AuthToken
+
+$AllDevices = Get-IDMDevices
+
+#change assigned profile for eahc device in list
+#TEST $Device = $devicList[0]
+foreach ($Device in $devicList)
+{
+    Write-Host ("Checking device: {0}..." -f $Device.DeviceName) -NoNewline -ForegroundColor White
+    #must get the device id from the display name
+    $DeviceID = $AllDevices | Where-Object { $_.deviceName -eq $Device.DeviceName } | Select-Object -ExpandProperty Id
+
+    If($null -eq $DeviceID)
+    {
+        Write-Host "Device Id not found" -ForegroundColor Red
+        Continue #skip to next device
+    }Else{
+        Write-Host ("Id: {0}" -f $DeviceID) -ForegroundColor Cyan
+    }
+    
+    Write-Host ("  |--Device is assigned to...") -NoNewline -ForegroundColor White
+    #get the current assigned user
+    $CurrentUser = Get-IDMDeviceAssignedUser -DeviceId $DeviceID -Passthru
+    
+    #check if the device is assigned to an admin account
+    If($CurrentUser.userPrincipalName -match $admincheck)
+    {
+        Write-Host ("an admin account: {0}" -f $CurrentUser.userPrincipalName) -ForegroundColor Yellow
+        
+        #if true, change the assigned profile
+        Write-Host ("  |--re-assigning to: {0}..." -f $Device.AssignedUser) -NoNewline -ForegroundColor White
+        Try{
+            Set-IDMDeviceAssignedUser -DeviceId $DeviceID -UPN $Device.AssignedUser
+            Write-Host ("Completed!") -ForegroundColor Green
+        }Catch{
+            Write-Host ("Failed: {0}" -f $_.Exception.Message) -ForegroundColor Red
+            Continue #skip to next device
+        }
+        
+    }ElseIf($CurrentUser.userPrincipalName -eq $Device.AssignedUser)
+    {
+        Write-Host ("the correct upn: {0}" -f $CurrentUser.userPrincipalName) -ForegroundColor Green
+    
+    }ElseIf( ($CurrentUser.userPrincipalName -ne $Device.AssignedUser) -and $AssignNonAdminUsers )
+    {
+        
+        Write-Host ("an another upn: {0}" -f $CurrentUser.userPrincipalName) -ForegroundColor Yellow
+        
+        #if true, change the assigned profile
+        Write-Host ("  |--re-assigning to: {0}..." -f $Device.AssignedUser) -NoNewline -ForegroundColor White
+        Try{
+            Set-IDMDeviceAssignedUser -DeviceId $DeviceID -UPN $Device.AssignedUser
+            Write-Host ("Completed!") -ForegroundColor Green
+        }Catch{
+            Write-Host ("Failed: {0}" -f $_.Exception.Message) -ForegroundColor Red
+            Continue #skip to next device
+        }
+
+    }Else{
+        
+        Write-Host ("to non-admin account: {0}" -f $CurrentUser.userPrincipalName) -ForegroundColor White
+    }
+    
+}
+
--- Tests/ChangePrimaryUserDeviceList.tests.ps1
+++ Tests/ChangePrimaryUserDeviceList.tests.ps1
@@ -1,65 +1,100 @@
-$DeviceUpdateList = @(
-    'DTOLAB-WKHV001',
-    'DTOLAB-WKHV002'
-)
+#import device
+[string]$ResourcePath = ($PWD.ProviderPath, $PSScriptRoot)[[bool]$PSScriptRoot]
+$devicList = Import-Csv "$ResourcePath\tests\devicelist.sample.csv"
+#$devicList = Import-Csv "$ResourcePath\devicelist.csv"
 
-#user id
-$AssignedUser = "tracyr@dtolab.ltd" 
+<#
+#set the admin check in regex
+^ = starts with
+adm- = the prefix for admin accounts
+#>
+$admincheck = '^adm-'
 
-# Set to $True to update the profile, $False to just see what would happen
-$DoProfileUpdate = $False
-#MAIN
+<# TESTS AGAINST REGEX
+'adm-frankjones@dtolab.ltd' -match $admincheck
+'adam.tool@dtolab.ltd' -match $admincheck
+'admintools@dtolab.ltd' -match $admincheck
+#>
+
+#Set to true to reassign non-admin users. 
+#$Otherwise assigned users in list are ignored unless there is an admin account assigne to device
+$AssignNonAdminUsers = $false
+
+#IMPORT MODULES
 #==============================================
 
 Install-Module Az.Accounts
 Install-Module Microsoft.Graph.Authentication
 Install-Module Microsoft.Graph.Applications
-Install-Module IDMCmdlets -MinimumVersion 1.0.2.2 -Force
+Install-Module IDMCmdlets
 
+#MAIN
+#==============================================
+#set the graph endpoint
 $Global:GraphEndpoint = 'https://graph.microsoft.com'
-Connect-MgGraph -NoWelcome
 
-$AllDevices = Get-IDMDevices -Platform Windows -Verbose
+#connect to the graph
+Connect-MgGraph
 
-#assigned profile for each device in list
-Write-Host "Found $($AllDevices.Count) devices in Intune." -ForegroundColor Cyan
+$AllDevices = Get-IDMDevices
 
-#loop through each device in the list
-#TEST $DeviceName = $DeviceUpdateList[0]
-foreach ($DeviceName in $DeviceUpdateList)
+#change assigned profile for eahc device in list
+#TEST $Device = $devicList[0]
+foreach ($Device in $devicList)
 {
-    Write-Host ("Detecting Assigned user for device {0}..." -f $DeviceName) -NoNewline
-    $DeviceID = $AllDevices | Where-Object { $_.deviceName -eq $DeviceName } | Select-Object -ExpandProperty Id
-    
+    Write-Host ("Checking device: {0}..." -f $Device.DeviceName) -NoNewline -ForegroundColor White
+    #must get the device id from the display name
+    $DeviceID = $AllDevices | Where-Object { $_.deviceName -eq $Device.DeviceName } | Select-Object -ExpandProperty Id
+
     If($null -eq $DeviceID)
     {
-        Write-Host "Device not found" -foregroundcolor Yellow
-        Continue
-    }
-    $DeviceAssignedUser = Get-IDMDeviceAssignedUser -DeviceId $DeviceID -Passthru
-    
-    If($null -eq $DeviceAssignedUser)
-    {
-        Write-Host "no assigned user found" -foregroundcolor Yellow
-    }Elseif($DeviceAssignedUser.UserPrincipalName -eq $AssignedUser){
-        Write-Host "assigned user is already correct" -foregroundcolor Green
-        Continue
+        Write-Host "Device Id not found" -ForegroundColor Red
+        Continue #skip to next device
     }Else{
-        Write-Host "assigned User: $($DeviceAssignedUser.UserPrincipalName)"
+        Write-Host ("Id: {0}" -f $DeviceID) -ForegroundColor Cyan
     }
     
-    If($DoProfileUpdate)
+    Write-Host ("  |--Device is assigned to...") -NoNewline -ForegroundColor White
+    #get the current assigned user
+    $CurrentUser = Get-IDMDeviceAssignedUser -DeviceId $DeviceID -Passthru
+    
+    #check if the device is assigned to an admin account
+    If($CurrentUser.userPrincipalName -match $admincheck)
     {
-        Write-Host ("|--Updating Assigned user to: {0}..." -f $DeviceName,$AssignedUser) -NoNewline
+        Write-Host ("an admin account: {0}" -f $CurrentUser.userPrincipalName) -ForegroundColor Yellow
+        
+        #if true, change the assigned profile
+        Write-Host ("  |--re-assigning to: {0}..." -f $Device.AssignedUser) -NoNewline -ForegroundColor White
         Try{
-            #$UpdatedUser = Get-IDMAzureUser -UPN $AssignedUser
-            Set-IDMDeviceAssignedUser -DeviceId $DeviceID -UPN $AssignedUser 
-            Write-Host "Success" -foregroundcolor Green
+            Set-IDMDeviceAssignedUser -DeviceId $DeviceID -UPN $Device.AssignedUser
+            Write-Host ("Completed!") -ForegroundColor Green
         }Catch{
-            Write-Host ("Error updating UPN for device {0}: {1}" -f $DeviceName,$_.Exception.message) -ForegroundColor Red
+            Write-Host ("Failed: {0}" -f $_.Exception.Message) -ForegroundColor Red
+            Continue #skip to next device
         }
+        
+    }ElseIf($CurrentUser.userPrincipalName -eq $Device.AssignedUser)
+    {
+        Write-Host ("the correct upn: {0}" -f $CurrentUser.userPrincipalName) -ForegroundColor Green
+    
+    }ElseIf( ($CurrentUser.userPrincipalName -ne $Device.AssignedUser) -and $AssignNonAdminUsers )
+    {
+        
+        Write-Host ("an another upn: {0}" -f $CurrentUser.userPrincipalName) -ForegroundColor Yellow
+        
+        #if true, change the assigned profile
+        Write-Host ("  |--re-assigning to: {0}..." -f $Device.AssignedUser) -NoNewline -ForegroundColor White
+        Try{
+            Set-IDMDeviceAssignedUser -DeviceId $DeviceID -UPN $Device.AssignedUser
+            Write-Host ("Completed!") -ForegroundColor Green
+        }Catch{
+            Write-Host ("Failed: {0}" -f $_.Exception.Message) -ForegroundColor Red
+            Continue #skip to next device
+        }
+
+    }Else{
+        
+        Write-Host ("to non-admin account: {0}" -f $CurrentUser.userPrincipalName) -ForegroundColor White
     }
+    
 }
-
-
-
